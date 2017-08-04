f37205399be3c75bf0c28ca8027bbe30e348f78a
defacid <defacid@gmail.com>
2017-08-03 23:05:51 -0400

    Adjusted timeline for UTC timezones

--- js/timeline.js
+++ js/timeline.js
@@ -7,7 +7,7 @@ function moveMarker() {
     var time = date.getTime();
 
     //var markerPosition = 0;
-    var markerPosition = time/100 % 86400 / 86400 * 100;
+    var markerPosition = (time/1000 - date.getTimezoneOffset()*60) % 86400 / 86400 * 100;
 
     document.getElementById("timeStamp").innerHTML = "" + hh + ":" + mm;
     //document.getElementById("timeStamp").innerHTML = markerPosition;
@@ -15,4 +15,11 @@ function moveMarker() {
     document.getElementById("marker").style.left = markerPosition + "vw";
 }
 
-setInterval(moveMarker, 100);
+moveMarker();
+setInterval(moveMarker, 1000);
+
+//NOTES
+//Cards fall to the bottom after their time passes for the day
+//Exclamation marker on/next to cards where the time has passed but the day hasn't ended
+//Reset streak for cards that haven't been completed in the time frame
+//Can set up "reset marker" to denote when the day should end and modify it in settings (helpful for night workers)
